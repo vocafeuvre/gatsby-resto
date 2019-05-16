08fdac5515e9591af2e8d490aa9c33d1ad302050
vocafeuvre <hirafura96@gmail.com>
2019-05-16 11:54:56 +0800

    Updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,35 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import css from "@emotion/css"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Layout>
+        <SEO title="404: Not found" />
+        <div
+            css={css`
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                padding-top: 100px;
+            `}
+        >
+            <h1
+                css={css`
+                    background-color: #aa3333;
+                `}
+            >
+                NOT FOUND
+            </h1>
+            <p
+                css={css`
+                    background-color: #aa3333;
+                `}
+            >
+                You just hit a route that doesn&#39;t exist... the sadness.
+            </p>
+        </div>
+    </Layout>
 )
 
 export default NotFoundPage
