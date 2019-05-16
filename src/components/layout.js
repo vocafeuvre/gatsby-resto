7dac8c42b78827d101ad643cc2feae381c07f18b
vocafeuvre <hirafura96@gmail.com>
2019-05-16 11:53:37 +0800

    Updated Layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,46 +8,59 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import { css } from "@emotion/core"
 
 import Header from "./header"
-import "./layout.css"
+import Footer from "./footer"
+
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+    <StaticQuery
+        query={graphql`
+            query SiteTitleQuery {
+                site {
+                    siteMetadata {
+						title
+                        description
+                        mobile
+                        email
+                        phone
+                        twitter
+                        facebook
+                        instagram
+                    }
+                }
+            }
+        `}
+        render={data => (
+            <>
+                <Header
+                    siteTitle={data.site.siteMetadata.title}
+                />
+				<main css={css`padding-bottom: 300px; min-height: 100%;`}>
+					{children}
+				</main>
+				<Footer 
+					siteTitle={data.site.siteMetadata.title}
+                    siteDescription={data.site.siteMetadata.description}
+                    siteContacts={{
+                        mobile: data.site.siteMetadata.mobile,
+                        email: data.site.siteMetadata.email,
+                        phone: data.site.siteMetadata.phone
+                    }}
+                    siteSocial={{
+                        twitter: data.site.siteMetadata.twitter,
+                        facebook: data.site.siteMetadata.facebook,
+                        instagram: data.site.siteMetadata.instagram
+                    }}
+				/>
+            </>
+        )}
+    />
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 }
 
 export default Layout
