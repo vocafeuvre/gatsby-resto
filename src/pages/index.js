12b0323f80239da8b27121eeab520c91661410a6
vocafeuvre <hirafura96@gmail.com>
2019-05-16 11:54:50 +0800

    Updated index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,16 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Intro from "../components/intro";
+import Second from "../components/second";
+import Third from "../components/third";
+import ActionCall from "../components/action-call";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => <Layout>
+	<Intro />
+	<Second />
+	<Third />
+	<ActionCall />
+</Layout>
 
 export default IndexPage
