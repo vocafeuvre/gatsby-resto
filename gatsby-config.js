aecfeed0189a03fe70f7620b4d4445f889ed2e71
vocafeuvre <hirafura96@gmail.com>
2019-05-16 11:33:45 +0800

    Personalized siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Antonio's`,
+    description: `Unlimited Chicken and Pasta for only PHP 299!.`,
+    author: `vocafeuvre`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/content/assets/`,
+        name: `assets`,
       },
     },
     `gatsby-transformer-sharp`,
